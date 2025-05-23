408ed5759747b00abbf24f1e2c2d15aa2a55560c
kaitlynpeterson <petersonk@carleton.edu>
2025-05-23 16:30:15 -0500

    letter base

--- word-games/app/wordleGame.tsx
+++ word-games/app/wordleGame.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import WordleBox from './wordleBox';
 
 export default function WordleGame() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Wordle</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <WordleBox> letter={"a"} </WordleBox>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
