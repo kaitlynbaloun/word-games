c0c39f30c9efca8fa58ee1b265f55164c6b3fc16
kaitlynpeterson <petersonk@carleton.edu>
2025-05-27 20:30:07 -0500

    start of keybaord

---
+++ word-games/app/keyBoard.tsx
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+import { View } from '@/components/Themed';
+import LetterKey, { KeyStatus } from './letterKey';
+
+const KEYBOARD_KEYS = [
+	['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+	['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+	['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL']
+]
+
+export interface IKeyBoardProps {
+}
+
+export default function KeyBoard( {  }: IKeyBoardProps ) {
+  	const keyBoardRows = [];
+
+	for (let rowIndex = 0; rowIndex < KEYBOARD_KEYS.length; rowIndex++) {
+		const letterRow = KEYBOARD_KEYS[rowIndex];
+		const letters = [];
+		for (let letterIndex = 0; letterIndex < letterRow.length; letterIndex++) {
+			letters.push(
+				<LetterKey
+					letter={letterRow[letterIndex]}
+					keyStatus={KeyStatus.Correct}
+				/>
+			)
+		}
+		keyBoardRows.push(
+			<View style={styles.keyBoardRow}>
+				{letters}
+			</View>
+		)
+	}
+	
+	return (
+		<View style={styles.keyBoard}>
+			{keyBoardRows}
+		</View>
+  	);
+}
+
+const styles = StyleSheet.create({
+	keyBoard: {
+    	justifyContent: 'center',
+    	alignItems: 'center',
+	},
+	keyBoardRow: {
+    	flexDirection: 'row',
+    	justifyContent: 'center',
+    	alignItems: 'center',
+		marginVertical: 4
+	}
+});
