7bac6a2a0c30df0ef9bb62b6d38a090326a8a393
Kaitlyn Baloun <19kaitlyn19@gmail.com>
2025-06-24 21:35:03 -0500

    add basic top level word logic and api call

---
+++ word-games/app/types/dictionaryApiResponse.ts
@@ -0,0 +1,25 @@
+interface Phonetic {
+    text: string;
+    audio: string;
+}
+
+interface Definition {
+    definition: string;
+    example: string;
+    synonyms: string[];
+    antonyms: string[];
+}
+
+export interface Meaning {
+    partOfSpeech: string;
+    definitions: Definition[];
+}
+
+export interface DictionaryResponse {
+    word: string;
+    phonetic: string;
+    phonetics: Phonetic[];
+    origin: string;
+    meanings: Meaning[];
+}
+
