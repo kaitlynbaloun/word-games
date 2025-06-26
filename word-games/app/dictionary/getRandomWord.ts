f232e90b1c35392237e069c2b2aa4d02570f8a45
Kaitlyn Baloun <19kaitlyn19@gmail.com>
2025-06-25 21:42:45 -0500

    remove async, remove old dictionart

--- word-games/app/dictionary/getRandomWord.ts
+++ word-games/app/dictionary/getRandomWord.ts
@@ -1,6 +1,6 @@
 import { dictionaryArray } from './dictionary';
 
-export const getRandomWord = async (): Promise<string | undefined> => {
+export const getRandomWord = (): string | undefined => {
     if (dictionaryArray.length === 0) {
         console.log('No 5-letter words found.');
         return;
