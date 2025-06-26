f03b7eb3c5a7610573118b04afd73c6a93b5961e
Kaitlyn Baloun <19kaitlyn19@gmail.com>
2025-06-25 21:32:31 -0500

    add in progress work

--- word-games/app/NeverWordGame.tsx
+++ word-games/app/NeverWordGame.tsx
@@ -48,7 +48,7 @@ export default function NeverWordGame() {
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <WordBoard enteredWords={enteredWords} correctWord={correctWord} inProgressWord={inProgressWord}/>
       <View style={styles.space} />
-      <KeyBoard guessedLetters={{'A': KeyStatus.Correct}} setInProgressWord={setInProgressWord}/>
+      <KeyBoard guessedLetters={{'A': KeyStatus.Correct}} setInProgressWord={setInProgressWord} inProgressWord={inProgressWord}/>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
