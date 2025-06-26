8dde4a609ca3571349cb300e72e1bd4f81c29179
Kaitlyn Baloun <19kaitlyn19@gmail.com>
2025-06-25 21:41:25 -0500

    fix random word generator to work on frontend

--- word-games/app/dictionary/getRandomWord.ts
+++ word-games/app/dictionary/getRandomWord.ts
@@ -1,27 +1,11 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import { dictionaryArray } from './dictionary';
 
-export const getRandomWord = (): string | undefined => {
-    const dictionaryPath = path.join(__dirname, 'dictionary.txt');
+export const getRandomWord = async (): Promise<string | undefined> => {
+    if (dictionaryArray.length === 0) {
+        console.log('No 5-letter words found.');
+        return;
+    }
 
-    fs.readFile(dictionaryPath, 'utf8', (error, fileData) => {
-        if (error) {
-            console.error('Error reading dictionary.txt:', error);
-            return;
-        }
-
-        const words = fileData
-            .split('\n')
-            .map(word => word.trim());
-
-        if (words.length === 0) {
-            console.log('No 5-letter words found.');
-            return;
-        }
-
-        const randomIndex = Math.floor(Math.random() * words.length);
-        return words[randomIndex];
-    });
-
-    return;
+    const randomIndex = Math.floor(Math.random() * dictionaryArray.length);
+    return dictionaryArray[randomIndex];
 }
