41f008a15102d665944793e31e7d6111066e6909
kaitlynpeterson <petersonk@carleton.edu>
2025-06-25 21:52:31 -0500

    update getDefinitionCall

--- word-games/app/getDefinitions.ts
+++ word-games/app/getDefinitions.ts
@@ -5,8 +5,8 @@ const DICTIONARY_API_BASE_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en
 
 export const getDefinitions = async (word: string): Promise<Meaning[]> => {
     try {
-        const dictionaryResponse = await axios.get<DictionaryResponse[]>(`${DICTIONARY_API_BASE_URL}${word}`)
-        if (dictionaryResponse.data.length < 1 || dictionaryResponse.data[0].word !==  word || dictionaryResponse.data[0].meanings.length < 1) {
+        const dictionaryResponse = await axios.get<DictionaryResponse[]>(`${DICTIONARY_API_BASE_URL}${word}`);
+        if (dictionaryResponse.data.length < 1 || dictionaryResponse.data[0].word.toLowerCase() !== word.toLowerCase() || dictionaryResponse.data[0].meanings.length < 1) {
             throw new Error('Unable to fetch definition');
         }
         return dictionaryResponse.data[0].meanings;
