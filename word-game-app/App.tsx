7db2653097ddc92446b042fb7677a9e44015c395
kaitlynpeterson <petersonk@carleton.edu>
2025-06-30 19:44:09 -0500

    try to remove expo router

---
+++ word-game-app/App.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import AppTabs from './components/AppTabs';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppTabs />
+    </NavigationContainer>
+  );
+}
